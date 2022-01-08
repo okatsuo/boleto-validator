68390b69d38ffb6382a493b8ef4482c1cb6d2fd2
okatsuo <rafael.goncanna@gmail.com>
2022-01-07 23:22:04 -0300

    feat: ensure boletoValidator return false if the code length isnt 44

--- src/boleto-validator.ts
+++ src/boleto-validator.ts
@@ -3,9 +3,11 @@ type IBoletoValidator = {
 }
 
 export class BoletoValidator implements IBoletoValidator {
+  bank_title_length = 44
   validate (digitableLine: string): boolean {
     const isValid = digitableLine.match(/^[0-9]+$/)
-    if (!isValid) return false
+    if (!isValid || digitableLine.length !== this.bank_title_length) return false
+
     return true
   }
 }
