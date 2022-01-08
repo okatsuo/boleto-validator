542e9d1a363afbf33e108bfffa6a04a6b60eb782
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 18:20:10 -0300

    refactor: move free numbers to variable to better understanding

--- src/service/boleto/index.ts
+++ src/service/boleto/index.ts
@@ -5,14 +5,21 @@ import { IHttpResponse } from '../protocols/httpResponse'
 import { IService } from '../protocols/service'
 
 export class BoletoService implements IService {
+  private readonly boletoBancarioLength = 47
+  private readonly boletoArrecadacaoLength = 48
+  private readonly boletoArrecadacaoInitialNumber = '8'
+
   constructor (private readonly boletoBancario: IBoletoValidator) {}
 
   handle = (req: Request): IHttpResponse => {
     const { digitableLine } = req.params
 
-    if (digitableLine.length === 47) {
+    if (digitableLine.length === this.boletoBancarioLength) {
       return this.boletoBancario.handle(digitableLine)
-    } else if (digitableLine.length === 48 && digitableLine[0] === '8') {
+    } else if (
+      digitableLine.length === this.boletoArrecadacaoLength &&
+       digitableLine[0] === this.boletoArrecadacaoInitialNumber
+    ) {
       return ok('boleto de arrecadação')
     }
 
