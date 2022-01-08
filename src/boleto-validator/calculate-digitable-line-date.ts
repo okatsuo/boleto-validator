883058643af97acf4fc396106074a1eea94f46e7
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 15:10:20 -0300

    srtyle: create variable to store the result

--- src/boleto-validator/calculate-digitable-line-date.ts
+++ src/boleto-validator/calculate-digitable-line-date.ts
@@ -5,6 +5,11 @@ export type ICalculateDate = {
 export class CalculateDate implements ICalculateDate {
   calculate = (value: string): string => {
     const date = new Date('10/07/1997')
-    return new Date(date.setTime(date.getTime() + (Number(value) * 24 * 60 * 60 * 1000))).toLocaleString().split(' ')[0]
+    const expirationDate = new Date(date.setTime(date.getTime() + (Number(value) * 24 * 60 * 60 * 1000)))
+
+    const formatExpirationDate = expirationDate
+      .toLocaleString().split(' ')[0]
+
+    return formatExpirationDate
   }
 }
