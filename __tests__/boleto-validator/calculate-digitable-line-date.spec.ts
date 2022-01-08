d91ba00e8f45edd7b403955ff632cce44545c5cb
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 15:13:52 -0300

    test: ensure CalculateDate works correctly

---
+++ __tests__/boleto-validator/calculate-digitable-line-date.spec.ts
@@ -0,0 +1,17 @@
+import { CalculateDate } from '../../src/boleto-validator/calculate-digitable-line-date'
+
+type SutType = {
+  sut: CalculateDate
+}
+
+const makeSut = (): SutType => {
+  const sut = new CalculateDate()
+  return { sut }
+}
+
+describe('Calculate date', () => {
+  it('should return with correct date', () => {
+    const { sut } = makeSut()
+    expect(sut.calculate('8861')).toBe('10/01/2022')
+  })
+})
