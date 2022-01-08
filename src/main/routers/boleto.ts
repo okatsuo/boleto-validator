6e05371a02a06e315323bb4c8acfc5fe730356a5
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 19:42:33 -0300

    fix: inject boletoArrecadacao to boletoService

--- src/main/routers/boleto.ts
+++ src/main/routers/boleto.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { BoletoService } from '../../service/boleto'
+import { makeBoletoArrecadacao } from '../factories/boleto-arrecadacao'
 import { makeBoletoValidator } from '../factories/boleto-validator'
 import { routerAdapter } from '../routerAdapter'
 
 export const boletoRouters = express.Router()
 
-boletoRouters.get('/boleto/:digitableLine', routerAdapter(new BoletoService(makeBoletoValidator())))
+boletoRouters.get('/boleto/:digitableLine', routerAdapter(new BoletoService(makeBoletoValidator(), makeBoletoArrecadacao())))
