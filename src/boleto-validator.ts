ac18492940deaa6345c32d976d0b8b997861cec1
okatsuo <rafael.goncanna@gmail.com>
2022-01-07 22:59:48 -0300

    feat: ensure boletoValidator return false if digitableLine isnt only number

---
+++ src/boleto-validator.ts
@@ -0,0 +1,10 @@
+type IBoletoValidator = {
+  validate: (digitableLine: string) => boolean
+}
+
+export class BoletoValidator implements IBoletoValidator {
+  validate (digitableLine: string): boolean {
+    const isValid = digitableLine.match(/^[0-9]+$/)
+    return !!isValid
+  }
+}
