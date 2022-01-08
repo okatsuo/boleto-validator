f38542ff3648093540e85dda49fc617276172c8e
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 10:17:18 -0300

    feat: ensure CalculateAmount returns correct value

---
+++ src/calculate-amount.ts
@@ -0,0 +1,13 @@
+export type ICalculateAmount = {
+  calculate: (value: string) => string
+}
+
+export class CalculateAmount {
+  calculate (value: string): string {
+    const amount = value.split('')
+    const cents = amount.splice(amount.length - 2, 2)
+    const amountToNumber = parseFloat(`${amount.join('')}.${cents.join('')}`)
+
+    return amountToNumber.toFixed(2)
+  }
+}
