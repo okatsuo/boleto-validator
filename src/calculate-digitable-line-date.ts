e347a383616cf17fa88ef4c2f790ff57cab0f48c
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 10:26:49 -0300

    refactor: move sut and stubs to a helper function

--- src/calculate-digitable-line-date.ts
+++ src/calculate-digitable-line-date.ts
@@ -1,10 +1,9 @@
-export type iCalculateDigitableLineDate = {
+export type ICalculateDigitableLineDate = {
   calculate: (value: string) => string
 }
 
-export class CalculateDigitableLineDate {
+export class CalculateDigitableLineDate implements ICalculateDigitableLineDate {
   calculate (value: string): string {
-    console.log('value:', value)
     const date = new Date('10/07/1997')
     return new Date(date.setTime(date.getTime() + (Number(value) * 24 * 60 * 60 * 1000))).toLocaleString().split(' ')[0]
   }
