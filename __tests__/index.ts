e8b29a6b7492ea4239ba98e14c4c262cd864cf78
okatsuo <rafael.goncanna@gmail.com>
2022-01-07 23:22:22 -0300

    test: ensure boletoValidator return false if the code length isnt 44

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,12 +1,18 @@
 import { BoletoValidator } from '../src/boleto-validator'
 
 describe('Boleto validator', () => {
-  it('should ', () => {
+  it('should return false if digitableLine isnt only numbers', () => {
     const boletoValidator = new BoletoValidator()
     expect(boletoValidator.validate('invalid_digitable_line')).toBe(false)
-    expect(boletoValidator.validate('48.273.8495018.182462')).toBe(false)
-    expect(boletoValidator.validate('48273849501818246A')).toBe(false)
-    expect(boletoValidator.validate('48,273,8495018,182462')).toBe(false)
-    expect(boletoValidator.validate('48-273-8495018-182462')).toBe(false)
+    expect(boletoValidator.validate('A1290001192110001210904475617405975870000002')).toBe(false)
+    expect(boletoValidator.validate('2129000119211000121090447561740597587000000A')).toBe(false)
+    expect(boletoValidator.validate('2129.000119.211000121090.447561740.5975870.000002')).toBe(false)
+    expect(boletoValidator.validate('21290-001192110-00121090447-5617405975870-000002')).toBe(false)
+  })
+
+  it('should return false if digitableLine isnt exactly 44 caracteres long', () => {
+    const boletoValidator = new BoletoValidator()
+    expect(boletoValidator.validate('212900011921100012109044756174059758700000021')).toBe(false)
+    expect(boletoValidator.validate('2129000119211000121090447561740597587000000')).toBe(false)
   })
 })
