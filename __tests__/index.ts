08883ed25e436f7a55d26b5cc982cc19d4d61371
okatsuo <rafael.goncanna@gmail.com>
2022-01-07 23:00:13 -0300

    test: ensure boletoValidator return false if digitableLine isnt only number

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,5 +1,12 @@
-describe('Name of the group', () => {
+import { BoletoValidator } from '../src/boleto-validator'
+
+describe('Boleto validator', () => {
   it('should ', () => {
-    expect('test').toBeDefined()
+    const boletoValidator = new BoletoValidator()
+    expect(boletoValidator.validate('invalid_digitable_line')).toBe(false)
+    expect(boletoValidator.validate('48.273.8495018.182462')).toBe(false)
+    expect(boletoValidator.validate('48273849501818246A')).toBe(false)
+    expect(boletoValidator.validate('48,273,8495018,182462')).toBe(false)
+    expect(boletoValidator.validate('48-273-8495018-182462')).toBe(false)
   })
 })
