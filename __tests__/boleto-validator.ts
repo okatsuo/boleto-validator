bb17ed71f8cf746b8f2428a315d289beacaa5d52
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 10:26:16 -0300

    refactor: move sut and stubs to a helper function

--- __tests__/boleto-validator.ts
+++ __tests__/boleto-validator.ts
@@ -2,31 +2,57 @@ import { BoletoValidator } from '../src/boleto-validator'
 import { ICalculateAmount } from '../src/calculate-amount'
 import { iCalculateDigitableLineDate } from '../src/calculate-digitable-line-date'
 
-class CalculateDigitableLineDateStub implements iCalculateDigitableLineDate {
-  calculate (value: string): string {
-    return 'valid_date'
+const makeCalculateDigitableLineDateStub = (): iCalculateDigitableLineDate => {
+  class CalculateDigitableLineDateStub implements iCalculateDigitableLineDate {
+    calculate (value: string): string {
+      return 'valid_date'
+    }
   }
+
+  return new CalculateDigitableLineDateStub()
 }
 
-class CalculateAmount implements ICalculateAmount {
-  calculate (value: string): string {
-    return 'valid_amount'
+const makeCalculateAmountStub = (): ICalculateAmount => {
+  class CalculateAmountStub implements ICalculateAmount {
+    calculate (value: string): string {
+      return 'valid_amount'
+    }
+  }
+
+  return new CalculateAmountStub()
+}
+
+type SutType = {
+  calculateDigitableLineDateStub: iCalculateDigitableLineDate
+  calculateAmountStub: ICalculateAmount
+  sut: BoletoValidator
+}
+
+const makeSut = (): SutType => {
+  const calculateDigitableLineDateStub = makeCalculateDigitableLineDateStub()
+  const calculateAmountStub = makeCalculateAmountStub()
+  const sut = new BoletoValidator(calculateDigitableLineDateStub, calculateAmountStub)
+
+  return {
+    calculateDigitableLineDateStub,
+    calculateAmountStub,
+    sut
   }
 }
 
 describe('Boleto validator', () => {
   it('should return false if digitableLine isnt only numbers', () => {
-    const boletoValidator = new BoletoValidator(new CalculateDigitableLineDateStub(), new CalculateAmount())
-    expect(boletoValidator.validate('invalid_digitable_line')).toBe(false)
-    expect(boletoValidator.validate('A3399340858500000011842498201013388610000065930')).toBe(false)
-    expect(boletoValidator.validate('0339934085850000001184249820101338861000006593A')).toBe(false)
-    expect(boletoValidator.validate('03399.34085850000001.18424982010133.8861000006.5930')).toBe(false)
-    expect(boletoValidator.validate('03399-340858500000011-842498201013388610000065-930')).toBe(false)
+    const { sut } = makeSut()
+    expect(sut.validate('invalid_digitable_line')).toBe(false)
+    expect(sut.validate('A3399340858500000011842498201013388610000065930')).toBe(false)
+    expect(sut.validate('0339934085850000001184249820101338861000006593A')).toBe(false)
+    expect(sut.validate('03399.34085850000001.18424982010133.8861000006.5930')).toBe(false)
+    expect(sut.validate('03399-340858500000011-842498201013388610000065-930')).toBe(false)
   })
 
   it('should return false if digitableLine isnt exactly 47 caracteres long', () => {
-    const boletoValidator = new BoletoValidator(new CalculateDigitableLineDateStub(), new CalculateAmount())
-    expect(boletoValidator.validate('212900011921100012109044756174059758700000021')).toBe(false)
-    expect(boletoValidator.validate('033993408585000000118424982010133886100000659301')).toBe(false)
+    const { sut } = makeSut()
+    expect(sut.validate('212900011921100012109044756174059758700000021')).toBe(false)
+    expect(sut.validate('033993408585000000118424982010133886100000659301')).toBe(false)
   })
 })
