76ef7ce49a93dbe9424955a9fd33a4cde585d885
okatsuo <rafael.goncanna@gmail.com>
2022-01-08 15:29:25 -0300

    feat: ensure to throw an error if value is not 10 characters long

--- src/boleto-validator/calculate-amount.ts
+++ src/boleto-validator/calculate-amount.ts
@@ -3,7 +3,10 @@ export type ICalculateAmount = {
 }
 
 export class CalculateAmount {
+  private readonly requiredAmountLenght = 10
   calculate = (value: string): string => {
+    if (value.length !== this.requiredAmountLenght) throw new Error('Amount must have 10 characters long')
+
     const amount = value.split('')
     const cents = amount.splice(amount.length - 2, 2)
     const amountToNumber = parseFloat(`${amount.join('')}.${cents.join('')}`)
